8af64e7773d27e142fdf992f1bdfb96a8dce2d25
André Terron <andrev.terron@gmail.com>
2018-01-22 02:44:07 -0200

    Accept streams as a promise result

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
 'use strict';
 
 var Promise = require('bluebird');
+var Stream = require('stream');
 
-var promisify = function(status, fn) {
-    if (!fn && typeof status === 'function') {
-        fn = status;
+var promisify = function(status, callback) {
+    if (!callback && typeof status === 'function') {
+        callback = status;
         status = null;
     }
     return function(req, res, next) {
         var result;
         try {
-            result = fn(req, res, next)
+            result = callback(req, res, next)
         } catch (e) {
             result = Promise.reject(e)
         }
 
-
+        // if it's null or undefined, do nothing
         if (result === null || result === undefined) {
             return;
         }
 
+        // accepts promises or values
         Promise.resolve(result)
             .then(function (reqResult) {
-                if (status) {
+                if (typeof status === 'number') {
                     res.status(status);
-                    if (status >= 300 && status < 400 && typeof reqResult === 'string') {
-                        res.redirect(reqResult);
-                        return;
-                    }
                 }
-                res.send(reqResult);
+                if (reqResult instanceof Stream) {
+                    // if it's an instance of a stream, pipe it to the response
+                    reqResult.pipe(res);
+                } else if (typeof status === 'number' && status >= 300 && status < 400 && typeof reqResult === 'string') {
+                    // if it's a redirect code, send a redirect
+                    res.redirect(reqResult);
+                } else {
+                    // otherwise, send the result
+                    res.send(reqResult);
+                }
             })
             .then(null, next); // this ensures to catch any errors on the function above
     }
 }
 
-promisify.errorHandler = function (err) {
+promisify.errorHandler = function (err, req, res, next) {
     if (err.stack) {
         console.error(err.stack);
     }
